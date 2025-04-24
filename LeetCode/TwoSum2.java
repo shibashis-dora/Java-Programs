a3442f54c5c3463f16ae310fa7f86d3d34f199ba
shibashis-dora <shibashisdora@gmail.com>
2025-04-24 23:23:01 +0530

    Adding LeetCode folder Adding first 3 programs

---
+++ LeetCode/TwoSum2.java
@@ -0,0 +1,53 @@
+/*
+1. TwoSUm
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+ 
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum2 { 
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int value = target - nums[i];
+            if (map.keySet().contains(value)) {
+                return new int[] {map.get(value), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[0];
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
+
+        int[] nums1 = {3,2,4};
+        int target1 = 6;
+        int[] result1 = twoSum(nums1, target1);
+        System.out.println("Indices: [" + result1[0] + ", " + result1[1] + "]");
+
+        int[] nums2 = {3,3};
+        int target2 = 6;
+        int[] result2 = twoSum(nums2, target2);
+        System.out.println("Indices: [" + result2[0] + ", " + result2[1] + "]");
+    }
+}
